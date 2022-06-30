ae1c958cdc6f86e1cc97584346cc7bca22b3c7c9
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-06-30 16:07:14 -0300

    30/06/2022

--- src/app/shared/models/tamanhos.spec.ts
+++ src/app/shared/models/tamanhos.spec.ts
@@ -2,6 +2,7 @@ import { Tamanhos } from './tamanhos';
 
 describe('Tamanhos', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new Tamanhos()).toBeTruthy();
   });
 });
