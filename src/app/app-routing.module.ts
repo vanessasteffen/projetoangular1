2ce8464045a9d9d75ef91a3c47c2f272b497f442
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-06-30 15:35:26 -0300

    30/06/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {FormDebugComponent} from "./shared/form-debug/form-debug.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component : AppComponent},
+   {path: 'pedidos', component : FormDebugComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
